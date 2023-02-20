8ba2c2ab450eb8bed3c36da9d6e7c3454a5c9e3e
sciencedude1990 <66385596+sciencedude1990@users.noreply.github.com>
2023-02-19 22:57:45 -0500

    Create serve_file.py

---
+++ serve_file.py
@@ -0,0 +1,134 @@
+# A very simple web server - good for running on a raspberry pi pico W
+
+# Imports
+import time
+import network
+import socket
+import sys
+import os
+import wifi_info     # The SSID and WIFI password
+    
+# Replace with your own SSID and WIFI password
+ssid = wifi_info.ssid
+wifi_password = wifi_info.wifi_password
+my_ip_addr = '192.168.0.22'
+
+# Please see https://docs.micropython.org/en/latest/library/network.WLAN.html
+# Try to connect to WIFI
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+
+# Specify the IP address
+wlan.ifconfig((my_ip_addr, '255.255.255.0', '192.168.0.1', '8.8.8.8'))
+
+# Connect
+wlan.connect(ssid, wifi_password)
+
+# Wait for connect or fail
+max_wait = 10
+while max_wait > 0:
+    if wlan.status() < 0 or wlan.status() >= 3:
+        break
+    max_wait -= 1
+    print('waiting for connection...')    
+    time.sleep(1)
+    
+# Handle connection error
+if wlan.status() != 3:
+    # Connection to wireless LAN failed
+    print('Connection failed')    
+    sys.exit()
+    
+else:
+    print('Connected')
+    status = wlan.ifconfig()
+    print( 'ip = ' + status[0] )
+    
+# Open socket
+addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
+s = socket.socket()
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+# Try to bind the socket
+try:
+    s.bind(addr)
+        
+except:
+    print('Bind Failed - you might need to wait a minute for things to clear up');    
+    sys.exit()
+    
+# Listen
+s.listen(1)
+print('listening on ', addr)
+
+# Timeout for the socket accept, i.e., s.accept()
+s.settimeout(5)
+
+# Listen for connections, serve up web page
+while True:
+    
+    # Handle connection error
+    if wlan.status() != 3:
+        # Connection to wireless LAN failed
+        print('Connection failed during regular operation')
+        sys.exit()
+        
+    # Main loop
+    accept_worked = 0
+    try:
+        print("Run s.accept()")
+        cl, addr = s.accept()
+        accept_worked = 1
+    except:
+        print('Timeout waiting on accept - reset the pico if you want to break out of this')
+        time.sleep(0.5)
+        
+    if accept_worked == 1:
+        try:
+            print('client connected from', addr)
+            request = cl.recv(1024)
+            print("request:")
+            print(request)
+            request = str(request)
+            
+            # Default response is error message                        
+            response = """<HTML><HEAD><TITLE>Error</TITLE></HEAD><BODY>Not found...</BODY></HTML>"""
+                    
+            # Parse the request for the filename - in the root directory
+            # Look for the "GET" text
+            base_file = request.find('GET /')            
+            if base_file == 2:
+                # Look for the "HTTP" text
+                end_name = request.find(' HTTP')
+                
+                if end_name != -1:
+                    # Get the filename
+                    f_name = request[7 : end_name]
+                    
+                    # Print the filename
+                    print("filename: " + f_name)
+                    
+                    try:                    
+                        # Get the file size, in bytes
+                        temp = os.stat(f_name)
+                    
+                        f_size_bytes = temp[6]
+                    
+                        fid = open(f_name, 'rb')
+                        response = fid.read()
+                        print(len(response))
+                        fid.close()
+                    except:
+                        print("Issue finding file...")
+                        
+                                
+            cl.send('HTTP/1.0 200 OK\r\nContent-Length: ' + str(len(response)) + '\r\nConnection: Keep-Alive\r\n\r\n')
+            cl.sendall(response)
+                
+            cl.close()
+            
+        except OSError as e:
+            cl.close()
+            print('connection closed')
+            
+
